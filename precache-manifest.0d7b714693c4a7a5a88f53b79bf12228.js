b776d1ada4f63c8de810bb480b80c8075c6e50ec
Rouzbeh Hatamy <roozbeh.hatamy@gmail.com>
2020-10-22 22:14:36 +0330

    Updates

---
+++ precache-manifest.0d7b714693c4a7a5a88f53b79bf12228.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352f8b2d8a67924b299c17d1536aff18",
+    "url": "/React-fc-phone-book/index.html"
+  },
+  {
+    "revision": "4c19e6f266e65be134f6",
+    "url": "/React-fc-phone-book/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "26088e2a2374c40f3cce",
+    "url": "/React-fc-phone-book/static/css/main.9523e6cf.chunk.css"
+  },
+  {
+    "revision": "4c19e6f266e65be134f6",
+    "url": "/React-fc-phone-book/static/js/2.caf5435c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-fc-phone-book/static/js/2.caf5435c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26088e2a2374c40f3cce",
+    "url": "/React-fc-phone-book/static/js/main.33ad18e0.chunk.js"
+  },
+  {
+    "revision": "186ce036574265413c8b",
+    "url": "/React-fc-phone-book/static/js/runtime-main.cdc78548.js"
+  },
+  {
+    "revision": "25da423a7b38f0b39027d495195db418",
+    "url": "/React-fc-phone-book/static/media/bg.25da423a.jpg"
+  },
+  {
+    "revision": "648605b9df9917bf501aaaa545e58043",
+    "url": "/React-fc-phone-book/static/media/hemi.648605b9.ttf"
+  },
+  {
+    "revision": "228e900b42fbd85eddfd4357728b8efe",
+    "url": "/React-fc-phone-book/static/media/hype.228e900b.mp4"
+  },
+  {
+    "revision": "b461335682c162ccd6121567f1101e94",
+    "url": "/React-fc-phone-book/static/media/logo-can.b4613356.png"
+  },
+  {
+    "revision": "d2f4aedaf0d56ead36518d8cf32ef775",
+    "url": "/React-fc-phone-book/static/media/logo-footer.d2f4aeda.png"
+  },
+  {
+    "revision": "c28c4f5fa21a1e139de6effcd4c1d41d",
+    "url": "/React-fc-phone-book/static/media/pic.c28c4f5f.jpg"
+  },
+  {
+    "revision": "702e00276a1096764d8cc77b02df3645",
+    "url": "/React-fc-phone-book/static/media/type2.702e0027.png"
+  },
+  {
+    "revision": "8f9286e279c8cc8cb7fb8ca32034ba40",
+    "url": "/React-fc-phone-book/static/media/type3.8f9286e2.png"
+  },
+  {
+    "revision": "4fcaf553c71792196ccd9ac2cb1dda78",
+    "url": "/React-fc-phone-book/static/media/type4.4fcaf553.png"
+  }
+]);
